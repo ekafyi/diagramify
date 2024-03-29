a0eadd1bdd56c033cfe2d0cc152ddda0e6214d4a
Eka <6597211+ekafyi@users.noreply.github.com>
2024-03-29 21:59:24 +0700

    feat: add env example

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+/**
+ * https://vitejs.dev/guide/env-and-mode#intellisense-for-typescript
+ */
+interface ImportMetaEnv {
+  readonly GOOGLE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
