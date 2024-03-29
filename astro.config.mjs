2d422208edf9e7267a3601215b5d44d68c1f9ded
Eka <6597211+ekafyi@users.noreply.github.com>
2024-03-29 21:34:42 +0700

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
+  integrations: [svelte()],
+  output: "server",
+  adapter: vercel(),
 });
