b895d5ad0ac20fc4f6e5fc8afb5b5df8e1905035
Eka <6597211+ekafyi@users.noreply.github.com>
2024-03-30 16:10:54 +0700

    refactor: add path alias + remove unused code

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,13 +5,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default getViteConfig({
   plugins: [svelte()],
-  // resolve: {
-  //   conditions: mode === "test" ? ["browser"] : [],
-  // },
   test: {
     globals: true,
     environment: "happy-dom",
     setupFiles: ["src/vitest-setup.js"],
-    // deps: { inline: ["@testing-library/svelte-vitest"] },
   },
 });
