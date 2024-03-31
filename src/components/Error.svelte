3f676e9ea8dae0c4f501c98fdc8e5ca59e194366
Eka <6597211+ekafyi@users.noreply.github.com>
2024-03-31 16:41:11 +0700

    feat: add ui error/alert

---
+++ src/components/Error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let title = "Error!";
+  export let msg = "";
+</script>
+
+<div role="alert" class="alert" aria-labelledby="error-heading">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    class="stroke-error shrink-0 w-6 h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    ></path>
+  </svg>
+  <div>
+    <p id="error-heading" class="font-bold">{title}</p>
+    {#if msg}
+      <p class="text-sm mt-1">
+        {msg}
+      </p>
+    {/if}
+  </div>
+</div>
