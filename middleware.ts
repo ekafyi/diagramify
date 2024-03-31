0aaec0590114bf459c9e772a0a3ffb7c415d08fc
Eka <6597211+ekafyi@users.noreply.github.com>
2024-04-01 02:13:25 +0700

    add blocked path + adjust rate limit settings

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,18 @@ import { kv } from "@vercel/kv";
 
 const ratelimit = new Ratelimit({
   redis: kv,
-  // 2 requests from the same IP in 100 seconds
-  limiter: Ratelimit.slidingWindow(2, "100 s"),
+  limiter: Ratelimit.slidingWindow(10, "3600 s"),
 });
 
-// Define which routes you want to rate limit
 export const config = {
   matcher: "/api/gemini",
 };
 
 export default async function middleware(request: Request) {
-  // You could alternatively limit based on user ID or similar
   const ip = ipAddress(request) || "127.0.0.1";
   const { success } = await ratelimit.limit(ip);
 
   return success
     ? next()
-    : Response.redirect(new URL("/blocked.html", request.url));
+    : Response.redirect(new URL("/api/blocked", request.url));
 }
